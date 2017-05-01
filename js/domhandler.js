c15d477eaae22ed8b17eaed172b685052c5ec85d
ollieOsh <sosinusi@gmail.com>
2017-05-01 17:17:10 -0500

    checkbox toggles

--- js/domhandler.js
+++ js/domhandler.js
@@ -1,12 +1,25 @@
 var clear = document.getElementById('clear'),
 	input = document.getElementById('usr-input'),
-	messages = document.getElementById('messages');
-console.log(messages.innerHTML);
+	messages = document.getElementById('messages'),
+	darkTheme = document.getElementById('darkTheme'),
+	nav = document.getElementById('nav'),
+	largeText = document.getElementById('makeLarge');
+
 clear.addEventListener('click', function(){
 	messages.innerHTML = '';
 	clear.setAttribute("disabled", true);
 })
 
+darkTheme.addEventListener('click', function(){
+	document.getElementsByTagName('body')[0].classList.toggle('dark');
+	nav.classList.toggle('navbar-inverse');
+	nav.classList.toggle('navbar-default');
+	messages.classList.toggle('dark-msg');
+})
+
+largeText.addEventListener('click', function(){
+	document.getElementsByTagName('body')[0].classList.toggle('larger');
+})
 
 Chatty.loadMessages();
 Chatty.showMessages();
