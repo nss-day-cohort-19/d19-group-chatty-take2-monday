dbe0160fd9995ba8fbf9c3f896da9414cfa522f7
cleverbek1991 <dilshod@Dilshods-MacBook-Pro.local>
2017-05-01 14:42:58 -0500

    KeyUp Event added

--- js/showmessages.js
+++ js/showmessages.js
@@ -1,9 +1,17 @@
 function showMessages(mess){
+	input = document.getElementById('usr-input');
+	output = document.getElementById('messages');
 
-	output = document.getElementById("messages");
-	for(i=0; i<mess.length;i++){
-		output.innerHTML += mess[i] + "<button id='i'>Delete</button><br>";
-	}
+	for(let i = 0; i < mess.length; i++){
+		output.innerHTML += mess[i] + `<button id="btn${i}">Delete</button><br>`;
+		};
+
+	input.addEventListener ('keyup', (key) => {
+		if (key.keyCode == 13) {
+			output.innerHTML += `${input.value} <button>Delete</button><br>`;
+			input.value = '';
+		};
+	})
 }
 
 Chatty.loadMessages(showMessages);
