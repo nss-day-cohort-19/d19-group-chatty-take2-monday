ec69cfce1b497c211223f3458bcb9d3b4e48b000
ollieOsh <sosinusi@gmail.com>
2017-05-01 17:24:11 -0500

    precious semicolons

--- js/domhandler.js
+++ js/domhandler.js
@@ -8,18 +8,18 @@ var clear = document.getElementById('clear'),
 clear.addEventListener('click', function(){
 	messages.innerHTML = '';
 	clear.setAttribute("disabled", true);
-})
+});
 
 darkTheme.addEventListener('click', function(){
 	document.getElementsByTagName('body')[0].classList.toggle('dark');
 	nav.classList.toggle('navbar-inverse');
 	nav.classList.toggle('navbar-default');
 	messages.classList.toggle('dark-msg');
-})
+});
 
 largeText.addEventListener('click', function(){
 	document.getElementsByTagName('body')[0].classList.toggle('larger');
-})
+});
 
 Chatty.loadMessages();
 Chatty.showMessages();
