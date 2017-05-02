a4cfbdb4de3b2af4e42cc94cf4f413de6c722fbd
Aarti Jaisinghani <jaisina@Aartis-MacBook-Air.local>
2017-05-02 12:50:09 -0500

    conflict

--- js/showmessages.js
+++ js/showmessages.js
@@ -11,28 +11,18 @@ var Chatty = (function(taco) {
         if (key.keyCode === 13) {
             //console.log("second mess", mess);
 
-<<<<<<< HEAD
-            output.innerHTML += `<div class="test">` + input.value + `<button id="btn${mess.length}" class="delete">Delete</button></div><br>`;
-            console.log("input.value", input.value);
-            mess.push(input.value);
-            //console.log("mess", mess);
-            Chatty.deleteMessages(mess);
-            //console.log("third mess", mess);
-=======
+
             output.innerHTML += `<div class="test"><p class="col-sm-6">${input.value}<p><button id="btn${mess.length}" class="delete">Delete</button></div>`;
             mess.push(input.value);
             Chatty.deleteMessages(mess);
             console.log("third mess", mess);
->>>>>>> master
             input.value = '';
 
         };
     })
-<<<<<<< HEAD
-    
-=======
+
     Chatty.deleteMessages(mess);
->>>>>>> master
+
 }
     return taco;
 })(Chatty);
