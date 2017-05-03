70b9a5302d4b1b502c6a2d45d99236ca5e75272d
Aarti Jaisinghani <jaisina@Aartis-MacBook-Air.local>
2017-05-03 17:11:24 -0500

    aj-monday

--- js/domhandler.js
+++ js/domhandler.js
@@ -4,6 +4,9 @@ var clear = document.getElementById('clear'),
     darkTheme = document.getElementById('darkTheme'),
     nav = document.getElementById('nav'),
     largeText = document.getElementById('makeLarge');
+    var  modalbgColor = document.getElementById("sel-bg-color");
+    var modalfontColor = document.getElementById("sel-font-color");
+
 
 clear.addEventListener('click', function(){
     messages.innerHTML = '';
@@ -27,6 +30,40 @@ darkTheme.addEventListener('click', function(){
 
 largeText.addEventListener('click', function(){
     document.getElementsByTagName('body')[0].classList.toggle('larger');
+});
+
+modalbgColor.addEventListener("change", function(){
+    console.log("bgevent", event.target.value);
+    if(event.target.value == "Dark"){
+        console.log("DARKKKKK");
+        document.getElementsByTagName('body')[0].classList.toggle('dark');
+        nav.classList.toggle('navbar-inverse');
+        nav.classList.toggle('navbar-default');
+        messages.classList.toggle('dark-msg');
+    } else 
+    {
+
+    }
+});
+
+modalfontColor.addEventListener("change", function(){
+    console.log("fontevent", event.target.value);
+     if(event.target.value == "red"){
+        console.log("DARKKKKK");
+        document.getElementsByTagName('body')[0].classList.toggle('red');
+        nav.classList.toggle('navbar-inverse');
+        nav.classList.toggle('navbar-default');
+        messages.classList.toggle('dark-msg');
+    } else if(event.target.value == "black"){
+        console.log("DARKKKKK");
+        document.getElementsByTagName('body')[0].classList.toggle('black');
+        nav.classList.toggle('navbar-inverse');
+        nav.classList.toggle('navbar-default');
+        messages.classList.toggle('dark-msg');
+
+        
+    }
+
 });
 
 Chatty.loadMessages();
