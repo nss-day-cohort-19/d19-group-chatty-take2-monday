ad8922c622e6a569db1f6f99884d56820292c0e9
ollieOsh <sosinusi@gmail.com>
2017-05-04 10:25:24 -0500

    save button works when editing message

--- js/edit.js
+++ js/edit.js
@@ -5,23 +5,39 @@ var Chatty = (function(taco) {
         var edit = document.getElementsByClassName("edit");
 
         for( i = 0; i < edit.length; i++) {
+
             edit.item(i).addEventListener("click", function(event){
-              console.log('test');
                 var editMessage = event.target.closest('div');
-                const p = editMessage.firstElementChild;
-                const input = document.createElement('input');
-                input.type = 'text';
-                input.setAttribute('class', 'col-sm-6 col-sm-offset-1 inputedit');
-                input.value = p.textContent;
-                editMessage.insertBefore(input, p);
-                editMessage.removeChild(p);
-                input.focus();
-                editMessage.childNodes[3].textContent = 'Save';
 
-                input.addEventListener("keyup", function(event){
+                if(this.textContent === 'Edit'){
+                    console.log('test');
+                    const p = editMessage.firstElementChild;
+                    const input = document.createElement('input');
+                    input.type = 'text';
+                    input.setAttribute('class', 'col-sm-6 col-sm-offset-1 inputedit');
+                    input.value = p.textContent;
+                    editMessage.insertBefore(input, p);
+                    editMessage.removeChild(p);
+                    input.focus();
+                    editMessage.childNodes[3].textContent = 'Save';
 
-                if (event.keyCode === 13) {
+                    input.addEventListener("keyup", function(event){
 
+                        if (event.keyCode === 13) {
+                            var editMessage = event.target.closest('div');
+                            const input = editMessage.firstElementChild;
+                            const p = document.createElement('p');
+                            p.textContent = input.value;
+                            p.setAttribute('class', 'col-sm-6');
+                            editMessage.insertBefore(p, input);
+                            editMessage.removeChild(input);
+                            editMessage.childNodes[3].textContent = 'Edit';
+                        }
+
+                    })
+                }
+                else if(this.textContent === 'Save'){
+                    console.log("hello");
 
                     var editMessage = event.target.closest('div');
                     const input = editMessage.firstElementChild;
@@ -32,7 +48,6 @@ var Chatty = (function(taco) {
                     editMessage.removeChild(input);
                     editMessage.childNodes[3].textContent = 'Edit';
                 }
-            })
             })
         }
     }
